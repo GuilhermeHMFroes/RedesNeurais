8f61105f9b62b072614a882dfa1cc60951a48717
GuilhermeFroes <guil.henrique.fr@gmail.com>
2022-11-22 22:46:17 -0300

    Questão 4

--- Lista de Exercícios 1/4.py
+++ Lista de Exercícios 1/4.py
@@ -2,18 +2,16 @@
 
 Exercício 4
 
-Faça o gráfico do exemplo anterior utilizando agora a função f(x) = x³ + x² + x + 3.
+Faça o gráfico do exemplo anterior utilizando agora a função f(x) = x3 + x2 + x + 3.
 
 """
 
-import numpy as np
-import matplotlib.pyplot as plt
-import random as rd
+from sympy import *
+from sympy.plotting import *
 
+if __name__ == '__main__':
 
+    x = Symbol('x')
 
-if '__main__':
-
-    d = 1
-
-    print(d)
+    # f(x) = x³ + x² + x + 3
+    plot(x**3 + x**2 + x + 3, (x, -10, 10))
